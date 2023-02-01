0cad5ca02c2970e40a2b7b9a273cf3206aec7d50
Maciej Tabiś <maciek.tabis@gmail.com>
2023-02-02 00:55:04 +0100

    Add timer

---
+++ src/components/Container/Container.js
@@ -0,0 +1,12 @@
+import Button from '../Button/Button'
+const Container = props => {
+    return (
+        <div>
+          <Button>Start</Button>
+        <Button>Stop</Button>
+        <Button>Reset</Button>
+        </div>
+    )
+}
+
+export default Container; 
